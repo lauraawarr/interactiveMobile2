e4de665592aa7d2d1d9de83230f0c098f62c50fc
Laura Warr <lauraawarr@gmail.com>
2017-03-17 12:05:33 -0400

    Updated content

--- www/scripts/controller.js
+++ www/scripts/controller.js
@@ -1,9 +1,15 @@
 var app = function(app){
 
-	app.makeController = function(stage, view){
+	app.makeController = function(stage, view, pages){
 
 		view.tabs.on("change", function(){
-			zog(view.tabs.text)
+			zog(view.tabs.value);
+			pages.go(view.page2, "right", "slide", 500);
+		});
+
+		view.tabs2.on("change", function(){
+			zog(view.tabs.value);
+			pages.go(view.page1, "left", "slide", 500);
 		});
 	}; //end makeController
 
